a323be4b4f4b7bce7d788548114f6154b2f3e15e
Heimi0807 <45440047+Heimi0807@users.noreply.github.com>
2018-11-29 21:06:10 -0600

    formulario editar tipo personal

--- resources/views/admin/configuraciones/recursohumano/tipopersonal/editartipopersonalrh.blade.php
+++ resources/views/admin/configuraciones/recursohumano/tipopersonal/editartipopersonalrh.blade.php
@@ -1,49 +1,42 @@
 @extends('admin.menuprincipal')
-@section('tittle', 'Administración académica')
+@section('tittle', 'Configuraciones/Recurso Humano')
 @section('content')
 
 <div class="box box-info">
-            <div class="box-header with-border">
-              <h3 class="box-title"><Strong>Tipo personal</Strong></h3>
-            </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-                          <div class="box-body">
-
-                 {!! Form::open(['route'=>'guardargrado', 'method'=>'POST','class'=>'form-horizontal']) !!}
-
-
-                 <div class="form-group">                                           
-                                                {!! Form::label('lbid', 'Código',['class'=>'col-sm-2 control-label']) !!}
-                                                <div class="col-sm-10">
-                                                {!! Form::text('txtid',null,['class'=>'form-control pull-right','placeholder'=>'Código','required']) !!}
-                                                </div>
-                </div>
-                 <div class="form-group">                                           
-                                                {!! Form::label('lbtipopersonal', 'Tipo personal',['class'=>'col-sm-2 control-label']) !!}
-                                                <div class="col-sm-10">
-                                                {!! Form::text('txttipopers',null,['class'=>'form-control pull-right','placeholder'=>'Tipo personal','required']) !!}
-                                                </div>
-                </div>
-
-                 
-                
-
-                                                  
-          </div>
-
-              <div class="box-footer" align="right">                
-                 {!! Form::submit('Actualizar',['class'=>'btn btn-primary ']) !!}
-                  <a href="{{route('listatipopersonalrh')}}" class="btn btn-default">Cancelar</a>
-              </div>
-
-                {!! Form::close() !!}
-              <!-- /.box-footer -->
-           
-          </div>
-
-
-
-
-
+  <div class="box-header with-border">
+    <h3 class="box-title"><Strong>Editar tipo personal: {{ $tipo->v_tipopersonal }}</Strong></h3>
+  </div>
+  <!-- /.box-header -->
+  @if(count($errors) > 0)
+      <div class="alert alert-danger" role="alert">
+        <ul>
+          @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+  <div class="box-body">
+    {!! Form::open(['route'=>['actualizartipopersonalrh', $tipo], 'method'=>'PUT','class'=>'form-horizontal']) !!}
+    <div class="form-group">                                           
+      {!! Form::label('lbid', 'Código',['class'=>'col-sm-2 control-label']) !!}
+      <div class="col-sm-10">
+        {!! Form::text('txtid',$tipo->id,['class'=>'form-control pull-right','placeholder'=>'Código','disabled','required']) !!}
+      </div>
+    </div>
+    <div class="form-group">                                           
+      {!! Form::label('lbtipopersonal', 'Tipo personal',['class'=>'col-sm-2 control-label']) !!}
+      <div class="col-sm-10">
+        {!! Form::text('v_tipopersonal',$tipo->v_tipopersonal,['class'=>'form-control pull-right','placeholder'=>'Tipo personal','required']) !!}
+      </div>
+    </div>                                              
+  </div>
+  <!-- form start -->
+  <div class="box-footer" align="right">                
+    {!! Form::submit('Actualizar',['class'=>'btn btn-primary ']) !!}
+    <a href="{{route('listatipopersonalrh')}}" class="btn btn-default">Cancelar</a>
+  </div>
+{!! Form::close() !!}
+<!-- /.box-footer -->
+</div>
 @endsection
